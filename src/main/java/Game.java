41c8e77dcc4c05aceee31442a5a969b0edb05ce1
imaheem <72090526+imaheem@users.noreply.github.com>
2023-10-27 14:48:43 +0100

    tests

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,45 +1,51 @@
 import java.util.Map;
 import java.util.Random;
+
 public class Game {
 
     private int gridSize;
     private Monster[][] monsters;
-    private int playerX, playerY;
+    private int playerX;
+    private int playerY;
     private int treasureX, treasureY;
     private boolean isGameOver;
     private Random random = new Random();
-
-    // Monster types and greetings
-    private final int ZOMBIE = 2;
-    private final int VAMPIRE = 3;
-    private final int GHOST = 4;
-    private final Map<Integer, String> monsterGreetings = Map.of(
-            ZOMBIE, "The Zombie groans!",
-            VAMPIRE, "The Vampire hisses!",
-            GHOST, "The Ghost wails!"
-    );
 
     public Game(int gridSize) {
         this.gridSize = gridSize;
 
         // Place player and treasure randomly
-        Random random = new Random();
+        setPlayerAndTreasurePositions();
+
+        monsters = new Monster[gridSize][gridSize];
+        initializeMonsters();
+    }
+
+    private void setPlayerAndTreasurePositions() {
         // Set treasure's location
         treasureX = random.nextInt(gridSize);
         treasureY = random.nextInt(gridSize);
 
-// Set player's initial position ensuring it's not on the treasure
+        // Set player's initial position ensuring it's not on the treasure
         do {
             playerX = random.nextInt(gridSize);
             playerY = random.nextInt(gridSize);
         } while (playerX == treasureX && playerY == treasureY);
+    }
 
-        monsters = new Monster[gridSize][gridSize];
+    void initializeMonsters() {
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                if ((i == playerX && j == playerY) || (i == treasureX && j == treasureY)) {
-                    continue;
+                if (i == playerX && j == playerY) {
+                    continue; // Skip player's cell
                 }
+                if (i == treasureX && j == treasureY) {
+                    continue; // Skip treasure's cell
+                }
+                if (monsters[i][j] != null) {
+                    continue; // Skip cells that already have monsters
+                }
+
                 if (random.nextDouble() < 0.1) {
                     int monsterType = random.nextInt(3);
                     switch (monsterType) {
@@ -74,17 +80,16 @@ public class Game {
                 break;
         }
         if (playerX == treasureX && playerY == treasureY) {
-            System.out.println("You found the treasure! You win!" );
+            System.out.println("You found the treasure! You win!");
             isGameOver = true;
         } else if (monsters[playerX][playerY] != null) {
             System.out.println(monsters[playerX][playerY].greet());
-            System.out.println("A monster caught you! You lose!" );
+            System.out.println("A monster caught you! You lose!");
             isGameOver = true;
         } else {
-            int distance = Math.abs(playerX - treasureX) + Math.abs(playerY - treasureY);
-            System.out.println("You are " + distance + " steps away from the treasure." );
+            int distance = calculateDistanceToTreasure();
+            System.out.println("You are " + distance + " steps away from the treasure.");
         }
-
     }
 
     public boolean isGameOver() {
@@ -95,14 +100,80 @@ public class Game {
         for (int y = 0; y < gridSize; y++) {
             for (int x = 0; x < gridSize; x++) {
                 if (playerX == x && playerY == y) {
-                    System.out.print("P " );
+                    System.out.print("P ");
                 } else if (monsters[x][y] != null) {
-                    System.out.print(monsters[x][y].getSymbol() + " " );
+                    System.out.print(monsters[x][y].getSymbol() + " ");
                 } else {
-                    System.out.print(". " );
+                    System.out.print(". ");
                 }
             }
             System.out.println();
         }
+    }
+
+    public int getPlayerY() {
+        return playerY;
+    }
+
+    public int getPlayerX() {
+        return playerX;
+    }
+
+    public int getTreasureX() {
+        return treasureX;
+    }
+
+    public int getTreasureY() {
+        return treasureY;
+    }
+
+    public void setMonsterAtPosition(int x, int y, Monster monster) {
+        if (isValidPosition(x, y)) {
+            monsters[x][y] = monster;
+        } else {
+            throw new IllegalArgumentException("Invalid coordinates: (" + x + "," + y + ")");
+        }
+    }
+
+    public Monster getMonsterAtPosition(int x, int y) {
+        if (isValidPosition(x, y)) {
+            return monsters[x][y];
+        } else {
+            throw new IllegalArgumentException("Invalid coordinates: (" + x + "," + y + ")");
+        }
+    }
+
+    public int getGridSize() {
+        return gridSize;
+    }
+
+    public void setPlayerPosition(int x, int y) {
+        if (isValidPosition(x, y)) {
+            playerX = x;
+            playerY = y;
+        } else {
+            throw new IllegalArgumentException("Invalid player position: (" + x + "," + y + ")");
+        }
+    }
+
+    public void setTreasurePosition(int x, int y) {
+        if (isValidPosition(x, y)) {
+            treasureX = x;
+            treasureY = y;
+        } else {
+            throw new IllegalArgumentException("Invalid treasure position: (" + x + "," + y + ")");
+        }
+    }
+
+    private boolean isValidPosition(int x, int y) {
+        return x >= 0 && x < gridSize && y >= 0 && y < gridSize;
+    }
+
+    // Method to calculate the Manhattan distance to the treasure
+    public int calculateDistanceToTreasure() {
+        // Calculate the Manhattan distance
+        int distanceX = Math.abs(playerX - treasureX);
+        int distanceY = Math.abs(playerY - treasureY);
+        return distanceX + distanceY;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,187 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class GameTest {
 
+    private Game game;
 
+    @BeforeEach
+    public void setUp() {
+        int gridSize = 5;
+        game = new Game(gridSize);
+        // Set known initial positions for testing.
+        game.setPlayerPosition(0, 0);
+        game.setTreasurePosition(1, 1);
+    }
+
+    @Test
+    public void testInitialization() {
+        assertNotNull(game);
+    }
+
+    @Test
+    public void testPlayerStartsAtValidLocation() {
+        assertEquals(0, game.getPlayerX());
+        assertEquals(0, game.getPlayerY());
+    }
+
+    @Test
+    public void testTreasureAtValidLocation() {
+        assertEquals(1, game.getTreasureX());
+        assertEquals(1, game.getTreasureY());
+    }
+
+    @Test
+    public void testPlayerAndTreasureDoNotStartAtSameLocation() {
+        assertNotEquals(game.getPlayerX(), game.getTreasureX());
+        assertNotEquals(game.getPlayerY(), game.getTreasureY());
+    }
+
+    @Test
+    public void testMonstersInitialization() {
+        for (int i = 0; i < game.getGridSize(); i++) {
+            for (int j = 0; j < game.getGridSize(); j++) {
+                if ((i == game.getPlayerX() && j == game.getPlayerY()) ||
+                        (i == game.getTreasureX() && j == game.getTreasureY())) {
+                    assertNull(game.getMonsterAtPosition(i, j), "Monster overlaps with player or treasure at (" + i + "," + j + ")");
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testMoveUp() {
+        game.setPlayerPosition(2, 2); // Set player's initial position
+        game.setTreasurePosition(4, 4); // Set treasure's position
+
+        int initialPlayerY = game.getPlayerY();
+
+        game.move("up");
+        int newPlayerY = game.getPlayerY();
+        int deltaY = Math.abs(initialPlayerY - newPlayerY);
+
+        assertTrue(deltaY <= 1, "Player's Y position has changed incorrectly after move: up");
+    }
+
+    @Test
+    public void testMoveDown() {
+        int initialPlayerY = game.getPlayerY();
+        game.move("down");
+        assertEquals(initialPlayerY + 1, game.getPlayerY());
+    }
+
+    @Test
+    public void testMoveLeft() {
+        game.setPlayerPosition(2, 2); // Set player's initial position
+        game.setTreasurePosition(4, 4); // Set treasure's position
+
+        int initialPlayerX = game.getPlayerX();
+
+        game.move("left");
+        int newPlayerX = game.getPlayerX();
+        int deltaX = Math.abs(initialPlayerX - newPlayerX);
+
+        assertTrue(deltaX <= 1, "Player's X position has changed incorrectly after move: left");
+    }
+
+    @Test
+    public void testBoundaryUp() {
+        while (game.getPlayerY() > 0) {
+            game.move("up");
+        }
+        int yPos = game.getPlayerY();
+        game.move("up");
+        assertEquals(yPos, game.getPlayerY(), "Player moved out of bounds!");
+    }
+
+    @Test
+    public void testBoundaryDown() {
+        while (game.getPlayerY() < game.getGridSize() - 1) {
+            game.move("down");
+        }
+        int yPos = game.getPlayerY();
+        game.move("down");
+        assertEquals(yPos, game.getPlayerY(), "Player moved out of bounds!");
+    }
+
+    @Test
+    public void testBoundaryLeft() {
+        while (game.getPlayerX() > 0) {
+            game.move("left");
+        }
+        int xPos = game.getPlayerX();
+        game.move("left");
+        assertEquals(xPos, game.getPlayerX(), "Player moved out of bounds!");
+    }
+
+    @Test
+    public void testBoundaryRight() {
+        while (game.getPlayerX() < game.getGridSize() - 1) {
+            game.move("right");
+        }
+        int xPos = game.getPlayerX();
+        game.move("right");
+        assertEquals(xPos, game.getPlayerX(), "Player moved out of bounds!");
+    }
+
+    @Test
+    public void testFindTreasure() {
+        while (!game.isGameOver()) {
+            if (game.getPlayerX() < game.getTreasureX()) {
+                game.move("right");
+            } else if (game.getPlayerX() > game.getTreasureX()) {
+                game.move("left");
+            } else if (game.getPlayerY() < game.getTreasureY()) {
+                game.move("down");
+            } else if (game.getPlayerY() > game.getTreasureY()) {
+                game.move("up");
+            }
+        }
+        assertTrue(game.isGameOver(), "Game should end when player finds the treasure.");
+    }
+
+    @Test
+    public void testMonsterCatch() {
+        Monster mockMonster = mock(Monster.class);
+        game.setMonsterAtPosition(game.getPlayerX(), game.getPlayerY() + 1, mockMonster);
+        game.move("down");
+        assertTrue(game.isGameOver(), "Game should end when a monster catches the player.");
+    }
+
+    @Test
+    public void testCalculateDistanceToTreasure() {
+        // Set the player's position
+        game.setPlayerPosition(2, 2);
+
+        // Set the treasure's position
+        game.setTreasurePosition(4, 4);
+
+        // Calculate the distance to the treasure
+        int distance = game.calculateDistanceToTreasure();
+
+        // The expected distance is 4 (2 steps right, 2 steps down)
+        assertEquals(4, distance);
+    }
+
+    @Test
+    public void testMonsterGreeting() {
+        int newY = game.getPlayerY() + 1;
+        if (newY >= game.getGridSize()) {
+            newY = game.getPlayerY() - 1;
+        }
+
+        Monster mockMonster = mock(Monster.class);
+        game.setMonsterAtPosition(game.getPlayerX(), newY, mockMonster);
+
+        if (newY < game.getPlayerY()) {
+            game.move("up");
+        } else {
+            game.move("down");
+        }
+
+        assertTrue(game.isGameOver(), "Expected a greeting when the player moves onto a monster's location.");
+    }
 }
